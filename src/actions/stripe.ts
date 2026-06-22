630b3a2e4458389d499d902e5b43f2da4d457657
RafalSprengel <sprengel.rafal@gmail.com>
2026-06-22 10:04:13 +0100

    changes payment method

--- src/actions/stripe.ts
+++ src/actions/stripe.ts
@@ -183,7 +183,8 @@ export async function createCheckoutSession(bookingData: BookingData) {
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "blik", "p24"],
+      payment_method_types: ["card"],
+      // payment_method_types: ["card", "blik", "p24"],
       locale: "en",
       line_items: [
         {
